0a7858eeae6139199164ed42b0d4e8cedca57727
Nguyễn Xuân Thịnh <xuanthinhkl1@gmail.com>
2026-06-03 20:42:08 +0700

    Add sound tool

---
+++ Assets/Scripts/Sound/SoundValue.cs
@@ -0,0 +1,28 @@
+namespace SoundManager
+{
+    public enum SoundType
+    {
+        BG_Music_Menu = 0,
+        BG_Music_Gameplay = 1,
+        Enemy_05_Attack = 2,
+        Enemy_06_Attack = 3,
+        Enemy_Freeze_Effect = 4,
+        Enemy_Dead = 5,
+        Unit_Dead = 6,
+        Unit_Shoot_01 = 7,
+        Unit_Shoot_03 = 8,
+        Unit_Shoot_04 = 9,
+        Unit_Shoot_05 = 10,
+        Unit_Shoot_06 = 11,
+        Unit_Shoot_07 = 12,
+        Unit_Shoot_08 = 13,
+        Unit_Select = 14,
+        Gold_Drop = 15,
+        Skill_01 = 16,
+        Skill_03_Airplane = 17,
+        Skill_03_Rocket = 18,
+        Skill_Select = 19,
+        Level_Complete_Win = 20,
+        Level_Complete_Lose = 21,
+    }
+}
