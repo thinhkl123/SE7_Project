928ea967cbe840d71cd7aa898be10ca616f49c05
TenHoiNgan <nduyminh123789@gmail.com>
2026-06-07 17:53:51 +0700

    Add win/lose scence

---
+++ Assets/Scripts/UI/UIType/LoseSceneUI.cs
@@ -0,0 +1,6 @@
+using UnityEngine.UI;
+
+public class LoseSceneUI : UICanvas
+{
+    public Button BackButton;
+}
---
+++ Assets/Scripts/UI/UIType/WinSceneUI.cs
@@ -0,0 +1,6 @@
+using UnityEngine.UI;
+
+public class WinSceneUI : UICanvas
+{
+    public Button BackButton;
+}
